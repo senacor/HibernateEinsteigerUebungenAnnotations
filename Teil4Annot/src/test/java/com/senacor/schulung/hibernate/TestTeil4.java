caa6dee90fc7db8a9c1b7a904972b42777d9b46a
mploed <michael.ploed@gmail.com>
2012-07-18 15:22:02 +0200

    Teil4 läuft auf JPA und Hibernate 4.1

--- Teil4Annot/src/test/java/com/senacor/schulung/hibernate/TestTeil4.java
+++ Teil4Annot/src/test/java/com/senacor/schulung/hibernate/TestTeil4.java
@@ -3,6 +3,7 @@ package com.senacor.schulung.hibernate;
 import com.senacor.schulung.hibernate.domain.Adresse;
 import com.senacor.schulung.hibernate.domain.Foto;
 import com.senacor.schulung.hibernate.domain.Person;
+import com.sun.tools.internal.ws.wsdl.framework.Entity;
 import org.dbunit.DBTestCase;
 import org.dbunit.PropertiesBasedJdbcDatabaseTester;
 import org.dbunit.dataset.DataSetUtils;
@@ -16,18 +17,20 @@ import org.hibernate.criterion.CriteriaSpecification;
 import org.hibernate.criterion.Projections;
 import org.hibernate.transform.ResultTransformer;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import java.io.File;
 import java.io.FileInputStream;
 import java.util.List;
 
 public class TestTeil4 extends DBTestCase {
 
-    private SessionFactory sf = null;
+    private EntityManagerFactory entityManagerFactory = null;
 
     public TestTeil4() {
         super();
-        sf = new Configuration()
-                .configure().buildSessionFactory();
+        entityManagerFactory = Persistence.createEntityManagerFactory("test");
         System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_DRIVER_CLASS, "org.hsqldb.jdbcDriver");
         System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_CONNECTION_URL, "jdbc:hsqldb:hsql://localhost/testdb");
         System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_USERNAME, "sa");
@@ -40,7 +43,7 @@ public class TestTeil4 extends DBTestCase {
 
     @org.junit.Test
     public void testProvokeNonUniqueObjectException() {
-        Session s = sf.getCurrentSession();
+        Session s = (Session)entityManagerFactory.createEntityManager().getDelegate();
         Transaction t = s.beginTransaction();
         //TODO Hier Code schreiben, der eine NonUniqueObjectException provoziert
         t.commit();
@@ -49,7 +52,7 @@ public class TestTeil4 extends DBTestCase {
 
     @org.junit.Test
     public void testAvoidNonUniqueObjectException() {
-        Session s = sf.getCurrentSession();
+        Session s = (Session)entityManagerFactory.createEntityManager().getDelegate();
         Transaction t = s.beginTransaction();
         //TODO Hier Code schreiben, der die oben provozierte NonUniqueObjectException vermeidet
         t.commit();
@@ -58,18 +61,18 @@ public class TestTeil4 extends DBTestCase {
 
     @org.junit.Test
     public void testProvokeLazyInitException() {
-        Session s = sf.getCurrentSession();
-        Transaction t = s.beginTransaction();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
         //TODO Hier Code schreiben, der eine LazyInitializationException provoziert
-        t.commit();
+        entityManager.getTransaction().commit();
     }
 
     @org.junit.Test
     public void testReattach() {
-        Session s = sf.getCurrentSession();
-        Transaction t = s.beginTransaction();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
         //TODO Hier Code schreiben, der ein Reattachment eines Objekts im Zustand "Detached" durchführt
-        t.commit();    
+        entityManager.getTransaction().commit();
     }
     
 }
